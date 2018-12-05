ac37afa530b966e8045b23578bf505a5862f342f
Kamil Toś <tosektosek@gmail.com>
2018-12-05 15:47:59 +0100

    initial commit;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Marker } from 'mapbox-gl';
+import { MarkerComponent } from 'ngx-mapbox-gl/lib/marker/marker.component';
+export const warsawCoordinates = [21.23400, 52.21890];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mapbox-playground';
+export class AppComponent implements OnInit {
+
+  markers = [];
+  ngOnInit() {
+
+  }
+
+  addMarker(event) {
+    if (event.lngLat && event.originalEvent.target.className !== 'marker') {
+      this.markers.push({ lngLat: event.lngLat });
+    }
+  }
+
+  handleMarkerClick(event) {
+    // console.log(event)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxMapboxGLModule.withConfig({
+      accessToken: 'pk.eyJ1IjoidG9zZWt0b3NlayIsImEiOiJjam9wdnJmenAwZWR3M3BucnVseWxhZ25jIn0.jjIvvTQ_ICGR6_HT7YQH8A',
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
